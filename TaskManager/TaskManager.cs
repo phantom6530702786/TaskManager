2972c441d21e1b72bc6b7f42d282f2d26d294c7f
Phantom456423467578567 <studiomitugame@gmail.com>
2026-01-02 20:14:57 +0700

    Init project

---
+++ TaskManager/TaskManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    internal class TaskManager
+    {
+        public static Dictionary<string, TaskModel> AllTask = new Dictionary<string, TaskModel>();
+    }
+}
---
+++ TaskManager/TaskModel.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace TaskManager
+{
+    class TaskChildren
+    {
+        [JsonProperty("attached")]
+        public List<TaskModel> Attached { get; set; } = new List<TaskModel>();
+    }
+    internal class TaskModel
+    {
+        [JsonProperty("id")]
+        public string ID { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("markers")]
+        public Dictionary<string, string> Markers { get; set; } = new Dictionary<string, string>();
+        [JsonProperty("children")]
+        public TaskChildren Children { get; set; } = new TaskChildren();
+        public string TimeCreatedString
+        {
+            get
+            {
+                return new DateTime(TimeCreated).ToString("yyyy-MM-dd");
+            }
+        }
+        public long TimeCreated { get; set; }
+        public long TimeStart { get; set; }
+        public long TimeEnd { get; set; }
+        public TaskModel(string title)
+        {
+            ID = Guid.NewGuid().ToString();
+            TimeCreated = DateTime.Now.Ticks;
+            this.Title = title;
+        }
+    }
+}
